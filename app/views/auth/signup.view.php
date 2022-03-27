77a73a702c71e716e731c31411e9f869bdbf19b5
EhfazAdnan <ehfaz15-167@diu.edu.bd>
2022-03-27 09:40:40 -0700

    7-signup-ui

--- app/views/auth/signup.view.php
+++ app/views/auth/signup.view.php
@@ -1,5 +1,37 @@
 <?php require views_path('partials/header'); ?>
 
-  <h1>Sign Up Page</h1>
+  <div class="container-fluid border col-lg-5 col-md-6 mt-5 p-2">
+      <center>
+        <h3><i class="fa fa-user"></i> User Signup</h3>
+        <div><?= esc(APP_NAME) ?></div>
+      </center>
+      <br>
+
+      <form action="" method="post">
+          <div class="input-group mb-3">
+              <span class="input-group-text">UserName</span>
+              <input type="text" class="form-control" placeholder="Username" name="username" id="username" autofocus>
+          </div>
+
+          <div class="input-group mb-3">
+              <span class="input-group-text">Email</span>
+              <input type="email" class="form-control" placeholder="email" name="email" id="email">
+          </div>
+
+          <div class="input-group mb-3">
+              <span class="input-group-text">Password</span>
+              <input type="text" class="form-control" placeholder="password" name="password" id="password">
+          </div>
+
+          <div class="input-group mb-3">
+              <span class="input-group-text">Retype Password</span>
+              <input type="text" class="form-control" placeholder="retype-password" name="retype-password" id="retype-password">
+          </div>
+
+          <button class="btn btn-primary float-end">Signup</button>
+          <button class="btn btn-danger">Cancel</button>
+      </form>
+
+  </div>
 
 <?php require views_path('partials/footer'); ?>
--- app/views/partials/nav.view.php
+++ app/views/partials/nav.view.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg navbar-light bg-light" style="min-width: 350px">
   <div class="container-fluid">
     <a class="navbar-brand" href="#"><?=esc(APP_NAME); ?></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
