5c9fc5474201ac469ea3bf873d8fae1e8527c2f6
EhfazAdnan <ehfaz15-167@diu.edu.bd>
2022-03-29 13:41:32 -0700

    10-product-UI

--- app/views/home.view.php
+++ app/views/home.view.php
@@ -5,16 +5,54 @@
 </div>
 
 <div class="d-flex">
-    <div style="height: 600px; overflow-y: scroll;" class="shadow-sm col p-4">
+    <div style="height: 600px;" class="shadow-sm col-6 p-4">
 
            <div class="input-group mb-3"> <h3>Items</h3>
              <input type="text" class="ms-3 form-control" name="search" id="search" placeholder="search items here" autofocus>
              <span class="input-group-text"><i class="fa fa-search"></i></span>
            </div>
 
+           <div class="js-products d-flex" style="height: 90%; overflow-y: scroll; flex-wrap: wrap;">
+
+              <div class="card m-4 border-0" style="min-width: 250px; max-width: 250px;">
+                  <a href="#">
+                    <img src="assets/images/image.jpg" class="w-100 rounded border">
+                  </a>
+
+                  <div class="p-4" style="font-size: 20px;">
+                    <div class="text-muted">CARAMEL MOOLATTE</div>
+                    <div class="">$5.00</div>
+                  </div>
+              </div>
+
+              <div class="card m-4 border-0" style="min-width: 250px; max-width: 250px;">
+                  <a href="">
+                    <img src="assets/images/caramel-moolatte.png" class="w-100 rounded border">
+                  </a>
+
+                  <div class="p-4" style="font-size: 20px;">
+                    <div class="text-muted">CARAMEL MOOLATTE</div>
+                    <div class="">$5.00</div>
+                  </div>
+              </div>
+
+              <div class="card m-4 border-0" style="min-width: 250px; max-width: 250px;">
+                  <a href="">
+                    <img src="assets/images/caramel-moolatte.png" class="w-100 rounded border">
+                  </a>
+
+                  <div class="p-4" style="font-size: 20px;">
+                    <div class="text-muted">CARAMEL MOOLATTE</div>
+                    <div class="">$5.00</div>
+                  </div>
+              </div>
+              
+
+            </div>
+
     </div>
 
-    <div class="col bg-light p-4 pt-2">
+    <div class="col-6 bg-light p-4 pt-2">
       <div><center><h3>Cart <div class="badge bg-primary rounded-circle">3</div></h3></center></div>
 
       <table class="table table-striped table-hover">
