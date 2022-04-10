bc482ff37d8171ebb6cc55008771d42acad29e39
EhfazAdnan <ehfaz15-167@diu.edu.bd>
2022-04-10 11:09:05 -0700

    16-database-connection

--- app/core/functions.php
+++ app/core/functions.php
@@ -17,3 +17,26 @@ function views_path($view){
 function esc($str){
     return htmlspecialchars($str);
 }
+
+function db_connect(){
+    $DBHOST = "localhost";
+    $DBNAME = "pos_db";
+    $DBUSER = "root";
+    $DBPASS = "";
+    $DBDRIVER = "mysql";
+
+    try{
+       $con = new PDO("mysql:host=localhost;dbname=pos_db", $DBUSER,$DBPASS);
+    }catch(PDOException $e){
+       echo $e->getMessage();
+    }
+
+    return $con;
+
+}
+
+db_connect();
+
+function query($query, $data = array()){
+
+}
