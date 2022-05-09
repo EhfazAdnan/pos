f2934cd90fc1c807b846f76d7b71f29bcfde561e
EhfazAdnan <ehfaz15-167@diu.edu.bd>
2022-05-09 10:47:15 -0700

    25-database-class

--- app/core/functions.php
+++ app/core/functions.php
@@ -22,38 +22,6 @@ function redirect($page){
     header("Location: index.php?pg=" . $page);
 }
 
-function db_connect(){
-    $DBHOST = "localhost";
-    $DBNAME = "pos_db";
-    $DBUSER = "root";
-    $DBPASS = "";
-    $DBDRIVER = "mysql";
-
-    try{
-       $con = new PDO("$DBDRIVER:host=$DBHOST;dbname=$DBNAME", $DBUSER,$DBPASS);
-    }catch(PDOException $e){
-       echo $e->getMessage();
-    }
-
-    return $con;
-
-}
-
-function query($query, $data = array()){
-   $con = db_connect();
-   $smt = $con->prepare($query);
-   $check = $smt->execute($data);
-
-   if($check){
-       $result = $smt->fetchAll(PDO::FETCH_ASSOC);
-       if(is_array($result) && count($result) > 0){
-           return $result;
-       }
-   }
-
-   return false;
-}
-
 function allowed_columns($data, $table){
     if($table == 'users'){
         $columns = [
